b81498e28edd9a49029a2d14b51188f0f424651e
“Your <g.petkov1996@gmail.com>
2023-10-05 23:38:39 +0300

    Change

--- LeapYear/leapyear.cbl
+++ LeapYear/leapyear.cbl
@@ -3,25 +3,27 @@
        ENVIRONMENT DIVISION.
        DATA DIVISION.
        WORKING-STORAGE SECTION.
-       01 WS-YEAR PIC 9(4) VALUE 1994.
-       01 WS-RESULT PIC 9.
+       01 WS-YEAR PIC 9(4).
+       01 WS-RESULT PIC A(3).
        PROCEDURE DIVISION.
        LEAP.
+           DISPLAY "Choose a year: ".
+           ACCEPT WS-YEAR 
            IF FUNCTION MOD (WS-YEAR, 4) = 0 THEN
               IF FUNCTION MOD (WS-YEAR, 100) = 0 THEN
                  IF FUNCTION MOD (WS-YEAR, 400) = 0 THEN
-                    MOVE 1 TO WS-RESULT
+                    MOVE "YES" TO WS-RESULT
                     DISPLAY WS-RESULT
                  ELSE 
-                      MOVE 0 TO WS-RESULT
+                      MOVE "YES" TO WS-RESULT
                     DISPLAY WS-RESULT
                  END-IF
               ELSE
-                 MOVE 1 TO WS-RESULT
+                 MOVE "YES" TO WS-RESULT
                  DISPLAY WS-RESULT
               END-IF
            ELSE 
-              MOVE 0 TO WS-RESULT
+              MOVE "NO" TO WS-RESULT
                  DISPLAY WS-RESULT  
            END-IF
            CONTINUE.
